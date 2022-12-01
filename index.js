b832e4484995f45a9b70a5210c0323b6db60e6ce
Igorinni <igortolmachyov7@gmail.com>
2022-12-01 15:16:39 +0300

    Add error handling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const write = require("./write.js");
 
 const shortId = require("shortid");
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
@@ -15,68 +15,117 @@ app.get("/", (req, res) => {
 });
 
 app.get(process.env.TASKS, (req, res) => {
-  const tasks = read();
+  try {
+    const tasks = read();
 
-  if (req.query.order === "asc")
-    tasks.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-  if (req.query.order === "desc")
-    tasks.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    /* if (req.query.order) {
+      tasks.sort((a, b) =>
+        req.query.order === "asc"
+          ? new Date(a.createdAt) - new Date(b.createdAt)
+          : new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    } */
 
-  let responseTasks = [...tasks];
+    if (req.query.order === "asc")
+      tasks.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+    if (req.query.order === "desc")
+      tasks.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
-  if (req.query.filterBy) {
-    const filterBy = req.query.filterBy === "done" ? true : false;
-    responseTasks = responseTasks.filter((item) => item.done === filterBy);
+    let responseTasks = [...tasks];
+
+    if (req.query.filterBy) {
+      const filterBy = req.query.filterBy === "done" ? true : false;
+      responseTasks = responseTasks.filter((item) => item.done === filterBy);
+    }
+
+    let displayTasks = [...responseTasks];
+
+    if (req.query.page) {
+      const lastTask = req.query.pp * req.query.page;
+      const firstTask = lastTask - req.query.pp;
+      displayTasks = responseTasks.slice(firstTask, lastTask);
+    }
+
+    const resObj = {
+      count: displayTasks.length,
+      tasks: displayTasks,
+    };
+
+    res.status(200).json(resObj);
+  } catch (error) {
+    res.status(500).json("Error on server");
+    console.log(error);
   }
-
-  let displayTasks = [...responseTasks];
-
-  if (req.query.page) {
-    const lastTask = req.query.pp * req.query.page;
-    const firstTask = lastTask - req.query.pp;
-    displayTasks = responseTasks.slice(firstTask, lastTask);
-  }
-
-  const resObj = {
-    count: displayTasks.length,
-    tasks: displayTasks,
-  };
-
-  res.json(resObj);
 });
 
 app.post(process.env.TASKS, (req, res) => {
-  const tasks = read();
-  const newTask = { uuid: shortId.generate(), ...req.body };
-  const newTasks = [...tasks, newTask];
-  write(newTasks);
-  res.json("Задача успешно добавлена");
+  try {
+    const tasks = read();
+    if (
+      !req.body.name ||
+      req.body.name.trim() === "" ||
+      !req.body.done ||
+      !req.body.createdAt
+    ) {
+      return res.status(422).json("Error 422");
+    }
+    const newTask = { uuid: shortId.generate(), ...req.body };
+    const newTasks = [...tasks, newTask];
+    write(newTasks);
+    res.status(201).json("Task added successfully");
+  } catch (error) {
+    res.status(500).json("Error on server");
+    console.log(error);
+  }
 });
 
 app.delete(`${process.env.TASKS}/:id`, (req, res) => {
-  const id = req.params.id;
-  const tasks = read();
-  const newTasks = tasks.filter((item) => item.id != id);
-  write(newTasks);
-  res.json("Задача успешно удалена");
+  try {
+    const id = req.params.id;
+    const tasks = read();
+    const newTasks = tasks.filter((item) => item.id != id);
+    write(newTasks);
+    res.status(204).json("Task deleted successfully");
+  } catch (error) {
+    res.status(500).json("Error on server");
+    console.log(error);
+  }
 });
 
 app.delete(process.env.TASKS, (req, res) => {
-  write([]);
-  res.json("Все задачи удалены");
+  try {
+    write([]);
+    res.status(204).json("All tasks are deleted");
+  } catch (error) {
+    res.status(500).json("Error on server");
+    console.log(error);
+  }
 });
 
 app.patch(`${process.env.TASKS}/:id`, (req, res) => {
-  const { name, done, createdAt } = req.body;
-  const tasks = read();
-  let task = tasks.find((item) => item.uuid === req.params.id);
-  task.name = name;
-  task.done = done;
-  task.createdAt = createdAt;
+  try {
+    if (
+      !req.body.name ||
+      req.body.name.trim() === "" ||
+      !req.body.done ||
+      !req.body.createdAt
+    ) {
+      return res.status(422).json("Error 422");
+    }
+    const { name, done, createdAt } = req.body;
+    const tasks = read();
+    let task = tasks.find((item) => item.uuid === req.params.id);
+    task.name = name;
+    task.done = done;
+    task.createdAt = createdAt;
 
-  write(tasks);
+    write(tasks);
 
-  res.json(task);
+    res.status(200).json("Update task");
+  } catch (error) {
+    res.status(500).json("Error on server");
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
