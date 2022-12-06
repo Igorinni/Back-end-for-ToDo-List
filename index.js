950e41fe39cfe27d1ecceadfe53db3f930b33f74
Igorinni <igortolmachyov7@gmail.com>
2022-12-06 19:07:03 +0300

    Create tasks model and migration

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const express = require("express");
 const config = require("config");
 const recursive = require("recursive-readdir-sync");
 
-
-
-const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
-/* const sequelize = new Sequelize("node_postgres", "root", "123456", {
-  dialect: "mysql",
-  host: "localhost"
-}); */
-
 const PORT = config.get("port") || 3001;
 const app = express();
+const db = require("./models/index");
 
-app.use(express.json());
+const beginтing = (async function () {
 
-recursive(`${__dirname}/src/routes`).forEach((file) =>
-  app.use("/", require(file))
-);
+  try {
+    await db.sequelize.sync();
+    await db.sequelize.authenticate();
 
-app.listen(PORT, () => {
-  console.log(`Good! Server starting on port ${PORT}...`);
-});
+    app.use(express.json());
+
+    recursive(`${__dirname}/src/routes`).forEach((file) =>
+      app.use("/", require(file))
+    );
+
+    app.listen(PORT, () => {
+      console.log(`Good! Server starting on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+}());
