d65e96a4991b6a66b26069e1cdc480ee14a5991c
Igorinni <igortolmachyov7@gmail.com>
2022-12-06 11:46:46 +0300

    Fix bugs in tasks.delete.js

--- src/routes/tasks.delete.js
+++ src/routes/tasks.delete.js
@@ -4,7 +4,8 @@ const tasksHelper = require("../utils/tasks-helper.js");
 
 router.delete("/tasks", async (req, res) => {
   try {
-    (await tasksHelper.read().length) === 0 &&
+    const arrayTasks = await tasksHelper.read();
+    arrayTasks.length === 0 &&
       res.status(422).json("Error: All tasks have already been deleted ");
     await tasksHelper.write([]);
     res.status(204).json("All tasks are deleted");
