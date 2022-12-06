38331d343e8a4f734c49709e3779ea49ce58d6bb
Igorinni <igortolmachyov7@gmail.com>
2022-12-06 13:55:38 +0300

    Install sequelize

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const config = require("config");
 const recursive = require("recursive-readdir-sync");
 
+
+
+const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+/* const sequelize = new Sequelize("node_postgres", "root", "123456", {
+  dialect: "mysql",
+  host: "localhost"
+}); */
+
 const PORT = config.get("port") || 3001;
 const app = express();
 
